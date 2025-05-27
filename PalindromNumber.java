4f07b3e699fc1cd7f0e5d4c804b3aad453a13bca
pratiksolkar <2023bit045@sggs.ac.in>
2025-05-27 23:53:10 +0530

    Given number is Palindrome or not

---
+++ PalindromNumber.java
@@ -0,0 +1,23 @@
+// Given number is Palindrome or not
+
+class PalindromNumber{
+  String isPalindrome(int num){
+    String strNum = Integer.toString(num);
+    int length = strNum.length();
+    
+    for(int i=0; i<=length/2; i++){
+        if(strNum.charAt(i)==strNum.charAt((length--)-1)){
+          continue;
+        }
+        else{
+          return "The num "+num+" is not Palindrome";
+        }
+    }
+    return "The num "+num+" is Palindrome";
+  }
+  public static void main( String args[]){
+    PalindromNumber obj = new PalindromNumber();
+    String result = obj.isPalindrome(12321);
+    System.out.println(result);
+  }
+}
