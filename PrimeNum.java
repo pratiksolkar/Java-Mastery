6ec2f3e595ea486996d8055c4dbd083e35174f9d
pratiksolkar <2023bit045@sggs.ac.in>
2025-06-01 09:03:16 +0530

    Given number is Prime or not

---
+++ PrimeNum.java
@@ -0,0 +1,25 @@
+// Given number is Prime or not
+
+class PrimeNum{
+  String isPrime(int num){
+    int count=0;
+    for(int i=1; i<=num; i++){
+      if(num%i==0){
+        count++;
+      }
+      else{
+      }
+    }
+    if(count==2){
+      return num+" is Prime";
+    }
+    else{
+      return num+" is not Prime";
+    } 
+  }
+  public static void main( String args[]){
+    PrimeNum obj = new PrimeNum();
+    String result = obj.isPrime(11);
+    System.out.println(result);
+  }
+}
